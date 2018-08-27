1203136130931ac54ac734a10909767066ddda5c
Thomas Ott <thomas.ott@greenchapter.de>
2018-08-27 12:22:36 +0200

    add inital bootstrap shell script

---
+++ macos_boostrap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env zsh
+#
+# Bootstrap script for setting up a new macos machine
+
+# Notes
+# Based on Gerald baeck Bootstrap Script
+# - https://github.com/geraldbaeck/dotfiles/blob/master/osx_scripts/osx_bootstrap.sh
+
+echo "Silence is golden"
