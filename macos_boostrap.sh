3dd840d711078f41ece8d0109324ffaabd4ce942
Thomas Ott <thomas.ott@greenchapter.de>
2018-08-27 13:01:01 +0200

    Add basic brew zsh and oh-my-zsh installing

--- macos_boostrap.sh
+++ macos_boostrap.sh
@@ -6,4 +6,22 @@
 # Based on Gerald baeck Bootstrap Script
 # - https://github.com/geraldbaeck/dotfiles/blob/master/osx_scripts/osx_bootstrap.sh
 
-echo "Silence is golden"
+echo "⚠️  Start bootstrapping"
+
+# get sudo rights for all the things
+sudo -v
+
+# Check for Homebrew, install if we don't have it
+if test ! $(which brew); then
+	echo "Installing homebrew..."
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update homebrew recipes
+brew update
+
+# Install zsh Bash
+brew install zsh zsh-completions
+
+# Basic oh-my-zsh Installation
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
