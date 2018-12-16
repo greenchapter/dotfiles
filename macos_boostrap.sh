bd186be9ed34af8e9fca4d4711c9fba3c827a58a
Thomas Ott <thomas.ott@greenchapter.de>
2018-12-16 12:44:03 +0100

    feat(brew): add brew and oh-my-zsh installation to bootstrap (#14)
    * Add basic brew zsh and oh-my-zsh installing
    * add yes no conditional to the script

--- macos_boostrap.sh
+++ macos_boostrap.sh
@@ -6,4 +6,39 @@
 # Based on Gerald baeck Bootstrap Script
 # - https://github.com/geraldbaeck/dotfiles/blob/master/osx_scripts/osx_bootstrap.sh
 
-echo "Silence is golden"
+echo "🚀  Start bootstrapping your new mac."
+
+# get sudo rights for all the things
+echo -n "Get sudo rights for all the things. Is this ok? (y/n) "
+read yesno < /dev/tty
+
+if [ "x$yesno" = "xy" ];then
+	sudo -v
+else
+	echo "Sorry, these script only runs with root rights. 🙈"
+	exit
+fi
+
+
+echo -n "Do you want to install Homebrew? (y/n) "
+read yesno < /dev/tty
+
+if [ "x$yesno" = "xy" ];then
+	# Check for Homebrew, install if we don't have it
+	if test ! $(which brew); then
+		echo "Installing homebrew..."
+		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	else
+		echo "You already have installed homebrew."
+	fi
+fi
+
+
+# Update homebrew recipes
+brew update
+
+# Install zsh Bash
+brew install zsh zsh-completions
+
+# Basic oh-my-zsh Installation
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
